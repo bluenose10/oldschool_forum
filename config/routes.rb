a1af2c3c099baea0ffcf29ca3281fc88a81033fa
Mark Moran <mark.moran4@btinternet.com>
2016-03-21 08:12:06 +0000

    repaired form file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
     devise_for :users
-   resources :messages
-   resources :messages do
+    resources :messages do
      resources :comments 
    end
     root 'messages#index'
